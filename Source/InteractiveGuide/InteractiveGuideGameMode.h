157b5b21981e3768592c6ebb6be1830cf69f8518
antori <antori@gmail.com>
2016-09-30 14:48:17 +0200

    Initial commit

---
+++ Source/InteractiveGuide/InteractiveGuideGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "InteractiveGuideGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INTERACTIVEGUIDE_API AInteractiveGuideGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
